f1c750d294dcddc9e2cd592578f1687aa987166d
Nate Dunn <hello@natedunn.net>
2026-02-23 13:08:08 -0600

    fix(release): automate npm and GitHub publishing

---
+++ .releaserc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  branches: ["main"],
+  tagFormat: "convex-zen-v${version}",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        changelogFile: "packages/convex-zen/CHANGELOG.md",
+      },
+    ],
+    [
+      "@semantic-release/npm",
+      {
+        pkgRoot: "packages/convex-zen",
+      },
+    ],
+    "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: [
+          "packages/convex-zen/package.json",
+          "packages/convex-zen/CHANGELOG.md",
+        ],
+        message:
+          "chore(release): convex-zen ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
+};
