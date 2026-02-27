24a0f0ed94d16ffea94f01d3878286ef4337f933
Nate Dunn <hello@natedunn.net>
2026-02-27 14:35:46 -0600

    fix(tanstack): remove accidental auth test code

--- apps/tanstack/src/lib/auth-client.ts
+++ apps/tanstack/src/lib/auth-client.ts
@@ -2,8 +2,6 @@ import { createTanStackAuthClient } from "convex-zen/tanstack-start-client";
 import { api } from "../../convex/_generated/api";
 import { authPluginMeta } from "../../convex/auth/plugin/metaGenerated";
 
-const test = Object.keys(api.auth.plugin.admin);
-
 export const authClient = createTanStackAuthClient({
 	convexFunctions: api.auth,
 	pluginMeta: authPluginMeta,
